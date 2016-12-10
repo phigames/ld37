c29e73f92a53fda13e85ef3b3320b86bf1f826a1
phigames <andreas.saeuberli@gmail.com>
2016-12-10 16:28:15 +0100

    .

--- web/resources.dart
+++ web/resources.dart
@@ -4,11 +4,14 @@ class Resources {
 
   static ImageElement imgFloor;
   static ImageElement imgBookshelve;
+  static ImageElement imgBedHead;
+  static ImageElement imgBedFeet;
 
   static void load() {
     imgFloor = new ImageElement(src: 'res/floor.png');
     imgBookshelve = new ImageElement(src: 'res/bookshelve.png');
-
+    imgBedHead = new ImageElement(src: 'res/bedHead.png');
+    imgBedFeet = new ImageElement(src: 'res/bedFeet.png');
   }
 
 }
--- web/tile.dart
+++ web/tile.dart
@@ -14,6 +14,13 @@ abstract class Tile {
 
   void move(int targetX, int targetY);
 
+  void draw() {
+    bufferContext.drawImage(image, positionX * Tile.WIDTH, positionY * Tile.HEIGHT);
+    drawOnTop();
+  }
+
+  void drawOnTop();
+
 }
 
 class TileBookshelve extends Tile {
@@ -31,21 +38,60 @@ class TileBookshelve extends Tile {
     }
   }
 
+  void drawOnTop() {
+
+  }
+
 }
 
 class TileBed extends Tile {
 
-  TileBed.Head(int positionX, int positionY) : super(positionX, positionY) {
-    image = Resources.imgBookshelve;
+  TileBed.Head(int positionX, int positionY, [Tile feetTile]) : super(positionX, positionY) {
+    if (feetTile != null) {
+      partnerTile = feetTile;
+      partnerTile.partnerTile = this;
+    }
+    image = Resources.imgBedHead;
+  }
+
+  TileBed.Feet(int positionX, int positionY, [Tile headTile]) : super(positionX, positionY) {
+    if (headTile != null) {
+      partnerTile = headTile;
+      partnerTile.partnerTile = this;
+    }
+    image = Resources.imgBedFeet;
   }
 
   void move(int targetX, int targetY) {
-    if (room.tiles[targetX][targetY] == null) {
-      room.tiles[positionX][positionY] = null;
-      room.tiles[targetX][targetY] = this;
-      positionX = targetX;
-      positionY = targetY;
+    int partnerTargetX = targetX - positionX + partnerTile.positionX;
+    int partnerTargetY = targetY - positionY + partnerTile.positionY;
+    print('$targetX, $targetY,    $partnerTargetX, $partnerTargetY');
+    if ((room.tiles[targetX][targetY] == null || room.tiles[targetX][targetY] == partnerTile) &&
+        (room.tiles[partnerTargetX][partnerTargetY] == null || room.tiles[partnerTargetX][partnerTargetY] == this)) {
+      if (targetX == partnerTile.positionX && targetY == partnerTile.positionY) {
+        room.tiles[partnerTile.positionX][partnerTile.positionY] = null;
+        room.tiles[partnerTargetX][partnerTargetY] = partnerTile;
+        partnerTile.positionX = partnerTargetX;
+        partnerTile.positionY = partnerTargetY;
+        room.tiles[positionX][positionY] = null;
+        room.tiles[targetX][targetY] = this;
+        positionX = targetX;
+        positionY = targetY;
+      } else {
+        room.tiles[positionX][positionY] = null;
+        room.tiles[targetX][targetY] = this;
+        positionX = targetX;
+        positionY = targetY;
+        room.tiles[partnerTile.positionX][partnerTile.positionY] = null;
+        room.tiles[partnerTargetX][partnerTargetY] = partnerTile;
+        partnerTile.positionX = partnerTargetX;
+        partnerTile.positionY = partnerTargetY;
+      }
     }
+  }
+
+  void drawOnTop() {
+
   }
 
 }
